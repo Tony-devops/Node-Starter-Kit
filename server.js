f389dfac423a8487e5518bcd7785ea6433157aab
tony-devops <ta.devopsuk@gmail.com>
2023-03-28 14:33:41 +0100

    week 1

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
 const express = require ('express');
 const app = express();
-const Port = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (req,res)=>{
    res.send(`Yay Tony, You are smashing it mate`);
 });
-app.listen(Port, ()=>{
-    console.log(`Server is listening on port ${Port}. Ready... Steady... Gooo`)
+
+app.get("/cyf", function (req, res) {
+  res.send("CYF is :10O");
+});
+
+app.get("/tony", function (req, res) {
+  res.send("Keep going. You can hack it:10O%");
+});
+
+app.get("/quote", function (req, res) {
+  res.send("It is not about size of the dog in the fight but the fight in the dog that matters");
+});
+
+app.get("/iman", function (req, res) {
+    // console.log(req);
+    // console.log(req.query)
+    // for (const key in req.query){
+    //     console.log(key)
+    // }
+  let amount = req.query.amount;
+    let test = req.query.test;
+      let hello = req.query.hello;
+//   console.log(searchQuery);
+//   res.send("Hello World! You searched for " + searchQuery +': 3 beautiful things');
+res.send(`${amount} ${test} ${hello}`)
+});
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on port ${PORT}. Ready... Steady... Gooo`)
 });
